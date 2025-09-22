df67813b6187d9c078992dd32d3a1c191a195247
Albert <afiguerola@gaddexapp.com>
2025-09-22 20:33:38 +0200

    Created CuentaTest class.

--- src/test/java/models/CuentaTest.java
+++ src/test/java/models/CuentaTest.java
@@ -1,7 +1,12 @@
 package models;
 
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CuentaTest {
+    @BeforeEach
+    void setUp() {
 
+    }
 }
