2801cc7b187aa1615841cee9dc379bcc0353724a
Albert <afiguerola@gaddexapp.com>
2025-10-06 19:11:18 +0200

    Created basic tests.

--- src/test/java/models/CuentaTest.java
+++ src/test/java/models/CuentaTest.java
@@ -1,12 +1,32 @@
 package models;
 
-import org.junit.jupiter.api.BeforeEach;
-
+import org.junit.jupiter.api.Test;
+import java.math.BigDecimal;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CuentaTest {
-    @BeforeEach
-    void setUp() {
 
+    @Test
+    void testNombreCuenta() {
+        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("123.47"));
+
+        String esperado = "Andres";
+        String real = cuenta.getPersona();
+
+        assertEquals(esperado, real);
+
+        assertTrue(real.equals("Andres"));
     }
+
+    @Test
+    void testSaldoCuenta() {
+        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("123.47"));
+
+        assertEquals(123.47, cuenta.getSaldo().doubleValue());
+
+        assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
+
+        assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);
+    }
+
 }
