32fd4dd4911f610123c125d5fccc4940334fde35
Albert <afiguerola@gaddexapp.com>
2025-10-13 19:30:43 +0200

    Using lambda expressions

--- src/test/java/models/CuentaTest.java
+++ src/test/java/models/CuentaTest.java
@@ -61,9 +61,9 @@ class CuentaTest {
     @Test
     void testDineroInsuficienteExceptionCuenta() {
         Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
-        Exception exception = assertThrows(DineroInsuficienteException.class, () -> {
-            cuenta.debito(new BigDecimal(1500));
-        });
+        Exception exception = assertThrows(DineroInsuficienteException.class, () ->
+                cuenta.debito(new BigDecimal(1500))
+        );
         String actual = exception.getMessage();
         String esperado = "Dinero insuficiente";
         assertEquals(esperado, actual);
@@ -89,27 +89,26 @@ class CuentaTest {
         Cuenta cuenta2 = new Cuenta("Andres", new BigDecimal("1500.8989"));
 
         Banco banco = new Banco();
-        banco.setNombre("Banco del estado");
         banco.addCuenta(cuenta1);
         banco.addCuenta(cuenta2);
 
+        banco.setNombre("Banco del Estado");
         banco.transferir(cuenta2, cuenta1, new BigDecimal(500));
 
-        assertEquals("1000.8989", cuenta2.getSaldo().toPlainString());
-        assertEquals("3000", cuenta1.getSaldo().toPlainString());
-
-        assertEquals(2, banco.getCuentas().size());
-        assertEquals("Banco del estado", cuenta1.getBanco().getNombre());
-
-        assertEquals("Andres", banco.getCuentas().stream()
-                .filter(c -> c.getPersona().equals("Andres"))
-                .findFirst()
-                .get()
-                .getPersona()
-        );
-
-        assertTrue(banco.getCuentas().stream()
-                .anyMatch(c -> c.getPersona().equals("Jhon Doe"))
+        assertAll(
+                () -> assertEquals("1000.8989", cuenta2.getSaldo().toPlainString()),
+                () -> assertEquals("3000", cuenta1.getSaldo().toPlainString()),
+                () -> assertEquals(2, banco.getCuentas().size()),
+                () -> assertEquals("Banco del Estado", cuenta1.getBanco().getNombre()),
+                () -> assertEquals("Andres", banco.getCuentas().stream()
+                        .filter(c -> c.getPersona().equals("Andres"))
+                        .findFirst()
+                        .get()
+                        .getPersona()
+                ),
+                () -> assertTrue(banco.getCuentas().stream()
+                        .anyMatch(c -> c.getPersona().equals("Jhon Doe"))
+                )
         );
     }
 
