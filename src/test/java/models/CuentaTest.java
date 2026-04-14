88f83d5888d919a44154bbffbf219214eaaaeecd
Albert <afiguerola@gaddexapp.com>
2026-04-14 09:39:52 +0200

    Using parameterized test

--- src/test/java/models/CuentaTest.java
+++ src/test/java/models/CuentaTest.java
@@ -3,6 +3,8 @@ package models;
 import exceptions.DineroInsuficienteException;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.math.BigDecimal;
 
@@ -287,6 +289,16 @@ class CuentaTest {
         assertNotNull(cuenta.getSaldo());
         assertEquals(900, cuenta.getSaldo().intValue());
         assertEquals("900.12345", cuenta.getSaldo().toPlainString());
+    }
+
+    @ParameterizedTest(name = "número {index} ejecutando con valor {0} - {argumentsWithNames}")
+    @ValueSource(strings = {"100", "200", "300", "500", "700", "1000", "1200"})
+    @DisplayName("Probando retiro de la cuenta")
+    void testDebitoCuenta(String monto) {
+        cuenta.debito(new BigDecimal(monto));
+
+        assertNotNull(cuenta.getSaldo());
+        assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);
     }
 
 }
